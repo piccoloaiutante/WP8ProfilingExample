d3b8443467fe944506d04c5a0af932344fee1831
Michele Capra <michele@orangecode.it>
2013-01-25 15:37:27 +0100

    Repository initialization

---
+++ OrangeCode.Profiling/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using OrangeCode.Profiling.Resources;
+
+namespace OrangeCode.Profiling
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
